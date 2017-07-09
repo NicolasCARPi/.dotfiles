29fe10d7c49009464f9d6df24a610b981b06ef8e
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-07-09 16:55:06 +0200

    add openbsd support

--- install.sh
+++ install.sh
@@ -2,20 +2,26 @@
 # http://github.com/NicolasCARPi/.dotfiles
 # install sane env
 
+# the ln version of OpenBSD doesn't have -i
+i=""
+if [ "$(uname)" != "OpenBSD" ]; then
+    i=" -i"
+fi
+
 # make the links
-ln -s -i ~/.dotfiles/vim ~/.vim
-mkdir -p ~/.vim/backup
-ln -s -i .dotfiles/zsh ~/.zsh
-# zsh will not read ~/.zsh/zshrc
-ln -s -i ~/.dotfiles/zsh/zshrc ~/.zshrc
-ln -s -i ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s -i ~/.dotfiles/Xdefaults ~/.Xdefaults
-ln -s -i ~/.dotfiles/mailcap ~/.mailcap
-ln -s -i ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/tmuxinator ~/.tmuxinator
-mkdir -p ~/.config
-ln -s -i ~/.dotfiles/config/awesome ~/.config/awesome
-ln -s -i ~/.dotfiles/config/mpd ~/.config/mpd
-ln -s ~/.dotfiles/config/bspwm ~/.config/bspwm
-ln -s ~/.dotfiles/config/sxhkd ~/.config/sxhkd
-ln -s ~/.dotfiles/octaverc ~/.octaverc
+ln -s $i $HOME/.dotfiles/vim $HOME/.vim
+mkdir -p $HOME/.vim/backup
+ln -s $i .dotfiles/zsh $HOME/.zsh
+# zsh will not read $HOME/.zsh/zshrc
+ln -s $i $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
+ln -s $i $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
+ln -s $i $HOME/.dotfiles/Xdefaults $HOME/.Xdefaults
+ln -s $i $HOME/.dotfiles/mailcap $HOME/.mailcap
+ln -s $i $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+ln -s $HOME/.dotfiles/tmuxinator $HOME/.tmuxinator
+mkdir -p $HOME/.config
+ln -s $i $HOME/.dotfiles/config/awesome $HOME/.config/awesome
+ln -s $i $HOME/.dotfiles/config/mpd $HOME/.config/mpd
+ln -s $HOME/.dotfiles/config/bspwm $HOME/.config/bspwm
+ln -s $HOME/.dotfiles/config/sxhkd $HOME/.config/sxhkd
+ln -s $HOME/.dotfiles/octaverc $HOME/.octaverc
