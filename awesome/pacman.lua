751b59a5eb658e64503e0327b4133c7367222fa5
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-01-30 02:06:20 +0100

    add list of packages on hover

--- awesome/pacman.lua
+++ awesome/pacman.lua
@@ -32,10 +32,23 @@ function pacman:show_number()
     )
 end
 
+function pacman:show_packages()
+    local cmd = "pacman -Qu"
+    local f = assert(io.popen(cmd))
+    local out = assert(f:read('*a'))
+    naughty.notify {
+        icon = "/home/ktr/.config/awesome/pacman.png",
+        text = out,
+        title = "Packages to update",
+        timeout = 15, hover_timeout = 0.5,
+        width = 210,
+    }
+end
+
 -- init
 pacman:show_number()
 
-pacman:connect_signal("mouse::enter", function() pacman:show_number() end)
+pacman:connect_signal("mouse::enter", function() pacman:show_packages() end)
 
 return pacman
 
