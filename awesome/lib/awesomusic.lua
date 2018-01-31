2d602e1b6fd3008acccf71510ef47ef95338a418
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-01-31 02:45:49 +0100

    use utf8 for play pause random

--- awesome/lib/awesomusic.lua
+++ awesome/lib/awesomusic.lua
@@ -1,5 +1,6 @@
 -- awesomusic.lua | an mpd widget for awesomewm
 -- by NicolasCARPi
+-- https://github.com/NicolasCARPi/.dotfiles
 -- requires "mpc" and "amixer"
 
 local awful = require("awful")
@@ -8,9 +9,6 @@ local watch = require("awful.widget.watch")
 local naughty = require("naughty")
 
 local awesomusic = wibox.widget.textbox()
-
---rand_icon = wibox.widget.imagebox()
---rand_icon:set_image(confdir .. "/lib/pacman.png")
 
 -- for some reason the | head -n 1 is not working so we need to split the stdout
 function awesomusic:split(input_str, sep)
@@ -30,30 +28,27 @@ end
 function awesomusic:show_song()
     watch("mpc status", 2,
         function(widget, stdout, stderr, exitreason, exitcode)
-            --local rand_icon = ""
-            local rand_txt = ""
-            out = awesomusic:split(stdout)
+            local rand_icon = ""
+            local icon = " ▶ "
+            local out = awesomusic:split(stdout)
+
             if out[0]:match("^volume") then
                 -- nothing is playing
                 widget:set_text("")
                 return false
             end
             if out[2]:match("random: on") then
-                rand_txt = " (Z)"
+                rand_icon = "⚡ "
+            end
+
+            if out[1]:match("paused") then
+                icon = " ⌷⌷ "
             end
             -- status is first line
-            status = out[0]
+            local status = out[0]
 
-            -- now get volume
-            --local cmd = "amixer get 'PCM'"
-            --local f = assert(io.popen(cmd))
-            --local amixer_out = assert(f:read('*a'))
-            --local amixer_out_split = awesomusic:split(amixer_out)
-            --local line_split = awesomusic:split(amixer_get[5], " ")
-            --local vol = line_split[4]
-            --local vol = "A"
-            --out = status .. " | " .. vol .. " | "
-            out = status .. rand_txt .. " "
+            out = icon .. status .. rand_icon .. " "
+
             widget:set_text(out)
         end,
         awesomusic
