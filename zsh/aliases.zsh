44b1884d5ea6b4bb9cf9c63c3086ae42d5362da1
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-02-20 14:30:15 +0100

    add user-dirs and merge zshrc.local

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,4 @@
 # common aliases
-alias r='tmux attach'
 alias ls='ls --color=auto'
 alias ll='ls -lA'
 alias lll='ls -lA|less'
@@ -11,18 +10,23 @@ alias c='clear'
 alias df='df -h'
 alias free='free -h'
 alias g='git'
+alias m='mutt'
+
+# custom aliases
 alias serve='python2 -m SimpleHTTPServer 8888'
 alias dmesg='dmesg -H'
-alias tesla='vncviewer 192.168.102.122'
-alias pielcommon='vncviewer 192.168.102.6'
-alias mvideopiel='vncviewer 192.168.102.14'
-alias bigasscpu='vncviewer 192.168.102.82'
-alias axiom='vncviewer 192.168.102.131'
-alias europa='vncviewer 192.168.102.134'
-alias symux='vncviewer 192.168.102.19'
-alias mvideodam='vncviewer 192.168.102.147'
-alias mvideobravo='vncviewer 192.168.102.163'
+alias mutt='~/.bin/mount-share.sh && cd $d && mutt && clear'
+alias eject="eject /dev/sr0"
+alias tmux='tmux -u'
+alias r='tmux attach'
+alias cdrip='abcde'
+alias snes='zsnes'
+alias nes='fceux'
+alias tv='vim /mnt/data/var/docs/tv.txt'
+
+# exported variables
 export dev=~/.dev
+export t=/mnt/data/
 
 # before it was d for Desktop
 if [[ $(hostname) == "titan" ]]; then
@@ -30,6 +34,3 @@ if [[ $(hostname) == "titan" ]]; then
 else
     export d=~/tmp
 fi
-alias mutt='~/.bin/mount-share.sh && cd $d && mutt && clear'
-alias m='mutt'
-export s=~/.share
