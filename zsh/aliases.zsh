e29dd5c1617a7eaaf139e58a9699346b03e91e11
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-03-10 22:00:10 +0100

    add aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -23,6 +23,11 @@ alias cdrip='abcde'
 alias snes='zsnes'
 alias nes='fceux'
 alias tv='vim /mnt/data/var/docs/tv.txt'
+alias utf8chk='find . -type f -exec file -bi {} \; -print|less'
+
+# apps
+alias fiji='~/.opt/Fiji.app/ImageJ-linux64'
+alias minecraft='java -Xmx3G -Xms3G -jar ~/.opt/Minecraft.jar'
 
 # exported variables
 export dev=~/.dev
