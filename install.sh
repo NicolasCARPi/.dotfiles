b554ae8ae71cb5fa96c8ffac7c4b67af7341300c
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-04-13 06:40:18 +0200

    make it idempotent

--- install.sh
+++ install.sh
@@ -2,35 +2,42 @@
 # https://github.com/NicolasCARPi/.dotfiles
 # install sane env
 
-# the ln version of OpenBSD doesn't have -i
-i=""
-if [ "$(uname)" != "OpenBSD" ]; then
-    i=" -i"
+headless=0
+if [ "$1" = '--headless' ]; then
+    headless=1
 fi
 
+cd "$HOME"
+
 # make the links
-ln -sf $i $HOME/.dotfiles/vim $HOME/.vim
-ln -sf $i .dotfiles/zsh $HOME/.zsh
+if [ ! -d "$HOME/.vim" ]; then
+    ln -sf "$HOME/.dotfiles/vim" "$HOME/.vim"
+fi
+if [ ! -d "$HOME/.zsh" ]; then
+    ln -sf .dotfiles/zsh "$HOME/.zsh"
+fi
 # zsh will not read $HOME/.zsh/zshrc
-ln -sf $i $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
-ln -sf $i $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
-ln -sf $i $HOME/.dotfiles/Xdefaults $HOME/.Xdefaults
-ln -sf $i $HOME/.dotfiles/mailcap $HOME/.mailcap
-ln -sf $i $HOME/.dotfiles/gitconfig $HOME/.gitconfig
-ln -sf $i $HOME/.dotfiles/tmuxinator $HOME/.tmuxinator
-ln -sf $i $HOME/.dotfiles/octaverc $HOME/.octaverc
-ln -sf $i $HOME/.dotfiles/pystartup $HOME/.pystartup
-ln -sf $i $HOME/.dotfiles/gemrc $HOME/.gemrc
+ln -sf "$HOME/.dotfiles/zsh/zshrc" "$HOME/.zshrc"
+ln -sf "$HOME/.dotfiles/tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$HOME/.dotfiles/Xdefaults" "$HOME/.Xdefaults"
+ln -sf "$HOME/.dotfiles/mailcap" "$HOME/.mailcap"
+ln -sf "$HOME/.dotfiles/gitconfig" "$HOME/.gitconfig"
+ln -sf "$HOME/.dotfiles/tmuxinator" "$HOME/.tmuxinator"
+ln -sf "$HOME/.dotfiles/octaverc" "$HOME/.octaverc"
+ln -sf "$HOME/.dotfiles/pystartup" "$HOME/.pystartup"
+ln -sf "$HOME/.dotfiles/gemrc" "$HOME/.gemrc"
 
 # config dir
-mkdir -p $HOME/.config
-ln -sf $i $HOME/.dotfiles/awesome $HOME/.config/awesome
-ln -sf $i $HOME/.dotfiles/ncmpcpp $HOME/.config/ncmpcpp
-ln -sf $i $HOME/.dotfiles/config/mpd $HOME/.config/mpd
-ln -sf $i $HOME/.dotfiles/user-dirs.dir $HOME/.config/user-dirs.dir
+mkdir -p "$HOME/.config"
+if [ $headless -eq 0 ]; then
+    ln -sf "$HOME/.dotfiles/awesome" "$HOME/.config/awesome"
+    ln -sf "$HOME/.dotfiles/ncmpcpp" "$HOME/.config/ncmpcpp"
+    ln -sf "$HOME/.dotfiles/config/mpd" "$HOME/.config/mpd"
+    ln -sf "$HOME/.dotfiles/user-dirs.dir" "$HOME/.config/user-dirs.dir"
+fi
 
 # create dirs for vim
-mkdir -p $HOME/.vim/backup
-mkdir -p $HOME/.vim/undodir
+mkdir -p "$HOME/.vim/backup"
+mkdir -p "$HOME/.vim/undodir"
 
 echo "All symlinks created. Now run :PluginInstall in vim"
